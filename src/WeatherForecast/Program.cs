edd3e67a504060e634bcdec3573a6a855ea6c5ac
Poncelet Grégoire <gregoire.poncelet@neuroglia.io>
2022-10-31 16:56:06 +0100

    first-commit

---
+++ src/WeatherForecast/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Fast.Components.FluentUI;
+using WeatherForecast.Contexts;
+using WeatherForecast.Interactors.SearchCityOrZipCode;
+using WeatherForecast.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+  builder.Services.AddSingleton<IWeatherDataReadOnlyRepository, WeatherDataReadOnlyRepository>();
+  builder.Services.AddSingleton<IWeatherDataWriteOnlyRepository, WeatherDataWriteOnlyRepository>();
+  builder.Services.AddSingleton<ISearchCityOrZipCode, SearchCityOrZipCode>();
+}
+else
+{
+  builder.Services.AddDbContext<WeatherDataContext>(options => options.UseSqlServer(connectionString));
+  builder.Services.AddScoped<IWeatherDataReadOnlyRepository, WeatherDataReadOnlyContext>();
+  builder.Services.AddScoped<IWeatherDataWriteOnlyRepository, WeatherDataWriteOnlyContext>();
+  builder.Services.AddScoped<ISearchCityOrZipCode, SearchCityOrZipCode>();
+}
+
+var app = builder.Build();
+app.UseExceptionHandler("/Error");
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseStaticFiles();
+app.UseEndpoints(endpoint => endpoint.MapControllers());
+app.UseEndpoints(endpoint => endpoint.MapRazorPages());
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
+
+public sealed partial class Program
+{
+}
---
+++ tests/WeatherForecast.Test/WeatherForecastContainer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace WeatherForecast.Test;
+
+[UsedImplicitly]
+public sealed class WeatherForecastContainer : HttpClient, IAsyncLifetime
+{
+  private static readonly X509Certificate Certificate = new X509Certificate2(WeatherForecastImage.CertificateFilePath, WeatherForecastImage.CertificatePassword);
+
+  private static readonly WeatherForecastImage Image = new();
+
+  private readonly IDockerNetwork _weatherForecastNetwork;
+
+  private readonly IDockerContainer _mssqlContainer;
+
+  private readonly IDockerContainer _weatherForecastContainer;
+
+  public WeatherForecastContainer()
+    : base(new HttpClientHandler
+    {
+      // Trust the development certificate.
+      ServerCertificateCustomValidationCallback = (_, certificate, _, _) => Certificate.Equals(certificate)
+    })
+  {
+    const string weatherForecastStorage = "weatherForecastStorage";
+
+    var mssqlConfiguration = new MsSqlTestcontainerConfiguration();
+    mssqlConfiguration.Password = Guid.NewGuid().ToString("D");
+    mssqlConfiguration.Database = Guid.NewGuid().ToString("D");
+
+    var connectionString = $"server={weatherForecastStorage};user id=sa;password={mssqlConfiguration.Password};database={mssqlConfiguration.Database}";
+
+    _weatherForecastNetwork = new TestcontainersNetworkBuilder()
+      .WithName(Guid.NewGuid().ToString("D"))
+      .Build();
+
+    _mssqlContainer = new TestcontainersBuilder<MsSqlTestcontainer>()
+      .WithDatabase(mssqlConfiguration)
+      .WithNetwork(_weatherForecastNetwork)
+      .WithNetworkAliases(weatherForecastStorage)
+      .Build();
+
+    _weatherForecastContainer = new TestcontainersBuilder<TestcontainersContainer>()
+      .WithImage(Image)
+      .WithNetwork(_weatherForecastNetwork)
+      .WithPortBinding(WeatherForecastImage.HttpsPort, true)
+      .WithEnvironment("ASPNETCORE_URLS", "https://+")
+      .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Path", WeatherForecastImage.CertificateFilePath)
+      .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Password", WeatherForecastImage.CertificatePassword)
+      .WithEnvironment("ConnectionStrings__DefaultConnection", connectionString)
+      .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(WeatherForecastImage.HttpsPort))
+      .Build();
+  }
+
+  public async Task InitializeAsync()
+  {
+    // It is not necessary to clean up resources immediately (still good practice). The Resource Reaper will take care of orphaned resources.
+    await Image.InitializeAsync()
+      .ConfigureAwait(false);
+
+    await _weatherForecastNetwork.CreateAsync()
+      .ConfigureAwait(false);
+
+    await _mssqlContainer.StartAsync()
+      .ConfigureAwait(false);
+
+    await _weatherForecastContainer.StartAsync()
+      .ConfigureAwait(false);
+  }
+
+  public async Task DisposeAsync()
+  {
+    await Image.DisposeAsync()
+      .ConfigureAwait(false);
+
+    await _weatherForecastContainer.DisposeAsync()
+      .ConfigureAwait(false);
+
+    await _mssqlContainer.DisposeAsync()
+      .ConfigureAwait(false);
+
+    await _weatherForecastNetwork.DeleteAsync()
+      .ConfigureAwait(false);
+  }
+
+  public void SetBaseAddress()
+  {
+    try
+    {
+      var uriBuilder = new UriBuilder("https", _weatherForecastContainer.Hostname, _weatherForecastContainer.GetMappedPublicPort(WeatherForecastImage.HttpsPort));
+      BaseAddress = uriBuilder.Uri;
+    }
+    catch
+    {
+      // Set the base address only once.
+    }
+  }
+}
